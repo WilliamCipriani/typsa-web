8e2d49132b91dddad5f9379d11fbf8989c324766
CIPRIANI NAVEDA WILLIAM JESUS <wjcipriani@typsa.es>
2023-06-13 10:56:11 -0500

    Agrego condiciones al contacto y funciona enviar correo

--- src/pages/contacto/index.js
+++ src/pages/contacto/index.js
@@ -3,16 +3,115 @@ import { Button, Form, Row, Col,  Container } from 'react-bootstrap';
 import HeadPage from '../../components/head'
 import styles from '@/styles/contacto.module.css'
 import  MapsGoogle from '../../components/maps'
+import { useState } from 'react';
+import axios from 'axios';
+
 
 
 export default function Contacto() {
     
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        phone: '',
+        company: '',
+        message: '',
+    });
+
+    const [formSubmitted, setFormSubmitted] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+      
+        // Validar el campo de teléfono para permitir solo números y 10 dígitos
+        if (name === 'phone') {
+          const onlyNums = value.replace(/[^0-9]/g, '');
+      
+          // Verificar si el valor solo contiene números y tiene 10 dígitos
+          if (/^[0-9]{0,9}$/.test(onlyNums)) {
+            setFormData((prevData) => ({
+              ...prevData,
+              [name]: onlyNums,
+            }));
+          }
+        } else if (name === 'email') {
+            // Validar el campo de correo electrónico con expresión regular
+            const validEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+        
+            if (validEmail) {
+              setFormData((prevData) => ({
+                ...prevData,
+                [name]: value,
+              }));
+            } 
+
+        }else {
+          setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+          }));
+        }
+      };
+
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        try {
+          // Validar si todos los campos están completos
+          if (!formData.name || !formData.email || !formData.phone || !formData.company || !formData.message) {
+            setErrorMessage('Por favor, complete todos los campos');
+            setTimeout(() => {
+                setErrorMessage('');
+              }, 3000);
+            return;
+          }else if (formData.phone.length < 9) {
+            setErrorMessage('El número de teléfono debe tener al menos 9 dígitos');
+            setTimeout(() => {
+                setErrorMessage('');
+              }, 3000);
+            return;
+          }
+
+          // Evitar envío repetido del formulario
+            if (formSubmitted) {
+                return;
+            }
+
+            setFormSubmitted(true);
+
+          await axios.post('http://localhost:3030/api/contact', formData);
+          
+          setSuccessMessage('El formulario se envió correctamente');
+          setTimeout(() => {
+            setSuccessMessage('');
+          }, 3000);
+          
+          setFormData({
+            name: '',
+            email: '',
+            phone: '',
+            company: '',
+            message: '',
+          });
+        } catch (error) {
+          console.error('Error al enviar el formulario:', error);
+          setErrorMessage('Ocurrió un error al enviar el formulario');
+          setTimeout(() => {
+            setErrorMessage('');
+          }, 3000);
+        } finally {
+            setFormSubmitted(false);
+            }
+      };
     
 
     return (
       <>
       <HeadPage />
-        <div className="d-flex flex-row justify-content-around text-center flex-nowrap mt-5 mb-5">
+      <div className={`d-flex justify-content-center mt-5`}>
+        <div className="d-flex flex-row justify-content-around text-center flex-nowrap mt-5 mb-5 w-50">
             <div className={`${styles.divisor}`}>
                 <h3 className="text-danger fw-bolder">VISÍTANOS</h3>
                 <p>Av. 28 de Julio 1044, Miraflores 15047</p>
@@ -28,39 +127,40 @@ export default function Contacto() {
                 <p>contacto@typsa.es</p>
             </div>
         </div>      
+        </div>
 
-        <Container className="mt-5 mb-5">
-            <Form className="mt-5">
+        <Container className="mt-5 mb-5 w-50">
+            
+            <Form className="mt-5" onSubmit={handleSubmit}>
                 <Row>
                     <Form.Group as={Col} className="mb-3 mt-5">
-                    <Form.Control type="text" placeholder="Nombre y Apellido" />
+                    <Form.Control type="text" placeholder="Nombre y Apellido" name="name" onChange={handleInputChange} value={formData.name}/>
                     </Form.Group>
                     <Form.Group as={Col} className="mb-3 mt-5">
-                    <Form.Control type="email" placeholder="Correo Electrónico" />
+                    <Form.Control type="email" placeholder="Correo Electrónico" name="email" onChange={handleInputChange} value={formData.email}/>
                     </Form.Group>
                 </Row>
                 <Row>
                     <Form.Group as={Col} className="mb-3 mt-5">
-                    <Form.Control type="tel" placeholder="Teléfono" />
+                    <Form.Control type="tel" placeholder="Teléfono" name="phone"  onChange={handleInputChange} value={formData.phone}/>
                     </Form.Group>
                     <Form.Group as={Col} className="mb-3 mt-5">
-                    <Form.Control type="text" placeholder="Empresa" />
+                    <Form.Control type="text" placeholder="Empresa" name="company"  onChange={handleInputChange} value={formData.company}/>
                     </Form.Group>
                 </Row>
                 <Form.Group className="mb-3 mt-5">
-                    <Form.Control as="textarea" rows={3} placeholder="Mensaje" />
+                    <Form.Control as="textarea" rows={3} placeholder="Mensaje" name ="message" onChange={handleInputChange} value={formData.message}/>
                 </Form.Group>
-                <Button variant="danger" type="submit">
+                {errorMessage && <p className="alert alert-danger">{errorMessage}</p>}
+                {successMessage && <p className="alert alert-success">{successMessage}</p> }
+                <Button variant="danger" type="submit" disabled={formSubmitted}>
                     Enviar
                 </Button>
             </Form>
-
+            
         </Container>
 
         <MapsGoogle />
-        
-    
-
       </>
     )
   }
