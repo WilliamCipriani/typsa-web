db438ed1c10796f8531b0f285caa9a1d027039db
William Cipriani <87953444+WilliamCipriani@users.noreply.github.com>
2023-06-15 00:35:40 -0500

    Update index.js

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -19,7 +19,7 @@ export default function LoginForm() {
       console.log(username, password);
   
   
-      const response = await fetch("http://localhost:3030/auth", {
+      const response = await fetch("https://typsaapi.azurewebsites.net/auth", {
 
         method: "POST",
         headers: {
